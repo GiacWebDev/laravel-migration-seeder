518969395e821965c13f614552d316cff500100b
GiacWebDev <giacobechini@gmail.com>
2023-11-21 16:47:04 +0100

    [edit] added info in migration

--- database/migrations/2023_11_21_150211_create_trains_table.php
+++ database/migrations/2023_11_21_150211_create_trains_table.php
@@ -16,7 +16,20 @@ return new class extends Migration
     public function up()
     {
         Schema::create('trains', function (Blueprint $table) {
+            // creata di default id
             $table->id();
+            // inserisco i campi che voglio passare al db
+            $table->string('company', 20);
+            $table->string('slug',20)->unique(); // lo slug è unico
+            $table->string('departure_station', 50);
+            $table->string('arrival_station', 50);
+            $table->time('departure_time');
+            $table->time('arrival_time');
+            $table->string('train_code', 10);
+            $table->integer('carriage_number');
+            $table->boolean('on_time')->default(true);
+            $table->boolean('canceled')->default(false);
+            // creata di default timestamp
             $table->timestamps();
         });
     }
