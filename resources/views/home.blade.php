db67380c14e4062a732ede0f273ee41b049cf2ed
GiacWebDev <giacobechini@gmail.com>
2023-11-22 18:42:25 +0100

    [edit] home.blade fake info

--- resources/views/home.blade.php
+++ resources/views/home.blade.php
@@ -20,30 +20,34 @@
         <table class="table">
             <thead>
                 <tr>
-                    <th scope="col">#</th>
-                    <th scope="col">First</th>
-                    <th scope="col">Last</th>
-                    <th scope="col">Handle</th>
+                    <th scope="col">Compagnia</th>
+                    <th scope="col">Slug</th>
+                    <th scope="col">S. Partenza</th>
+                    <th scope="col">S. Arrivo</th>
+                    <th scope="col">Orario partenza</th>
+                    <th scope="col">Orario ritorno</th>
+                    <th scope="col">Codice treno</th>
+                    <th scope="col">Carrozza n°</th>
+                    <th scope="col">In orario</th>
+                    <th scope="col">Cancellato</th>
                 </tr>
             </thead>
             <tbody>
-                <tr>
-                    <th scope="row">1</th>
-                    <td>Mark</td>
-                    <td>Otto</td>
-                    <td>@mdo</td>
-                </tr>
-                <tr>
-                    <th scope="row">2</th>
-                    <td>Jacob</td>
-                    <td>Thornton</td>
-                    <td>@fat</td>
-                </tr>
-                <tr>
-                    <th scope="row">3</th>
-                    <td colspan="2">Larry the Bird</td>
-                    <td>@twitter</td>
-                </tr>
+                @foreach ($trains as $train_data)
+                    <tr>
+                        <td>{{ $train_data->company }}</td>
+                        <td>{{ $train_data->slug }}</td>
+                        <td>{{ $train_data->departure_station }}</td>
+                        <td>{{ $train_data->arrival_station }}</td>
+                        <td>{{ $train_data->departure_time }}</td>
+                        <td>{{ $train_data->arrival_time }}</td>
+                        <td>{{ $train_data->train_code }}</td>
+                        <td>{{ $train_data->carriage_number }}</td>
+                        <td>{{ $train_data->on_time }}</td>
+                        <td>{{ $train_data->canceled }}</td>
+
+                    </tr>
+                @endforeach
             </tbody>
         </table>
 
